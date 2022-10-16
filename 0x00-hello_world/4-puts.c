54b0219d9d1b1ff21d6e237a7b6df44081520f13
Swaray10 <aswaray650@gmail.com>
2022-10-16 17:59:15 +0000

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main -print a line of code using puts
+ * main -print a line of code
  *
- * return: 0
+ * return: 0 unless otherwise
  */
 int main(void)
 {
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - size doesnt matter
+ * main -size doesnt matter
  *
  * return: 0 unless otherwise
  */
@@ -8,8 +8,8 @@ int main(void)
 {
 	printf("size of a char: %ld byte(s) \n", sizeof(char));
 	printf("size of an int: %ld byte(s) \n", sizeof(int));
-	printf("size of an long int: %ld byte(s) \n", sizeof(long int));
-	printf("size of an long long int: %ld byte(s) \n", sizeof(long long int));
-	printf("size of an float: %ld byte(s) \n", sizeof(float));
+	printf("size of a long int: %ld byte(s) \n", sizeof(long int));
+	printf("size of a long long int: %ld byte(s) \n", sizeof(long long int));
+	printf("size of a float: %ld byte(s) \n", sizeof(float));
 	return (0);
 }
