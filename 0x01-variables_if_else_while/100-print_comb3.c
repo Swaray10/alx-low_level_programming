4b075ce942c55edc7a1d8f7d298927cf47a3c768
Swaray10 <aswaray650@gmail.com>
2022-10-17 16:33:16 +0000

    finally done going to sleeeeep

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - combinations
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int dig1, dig2;
+
+	for (dig1 = 0; dig1 < 9; dig1++)
+	{
+		for (dig2 = dig1 + 1; dig2 < 10; dig2++)
+		{
+			putchar((dig1 % 10) + '0');
+			putchar((dig2 % 10) + '0');
+
+			if (dig1 == 8 && dig2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
